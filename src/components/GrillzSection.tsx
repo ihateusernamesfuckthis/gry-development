b7017a2c9020a19b8ae091ecbd55f62399d37864
peterbangpetterson <peterbang100@gmail.com>
2025-11-12 14:19:43 +0100

    fixet grillz component - tilføjet mobil layout og indsat nye billeder på desktop/mobile

--- src/components/GrillzSection.tsx
+++ src/components/GrillzSection.tsx
@@ -3,86 +3,148 @@ import Link from "next/link";
 
 export default function GrillzSection() {
   return (
-    <section className="self-stretch inline-flex flex-col justify-start items-start gap-3">
-      {/* Section Header */}
-      <div className="self-stretch pr-[5px] pt-4 bg-white flex flex-col lg:inline-flex lg:flex-row justify-start items-start lg:items-end gap-1 lg:gap-3.5 sticky top-0 z-10">
-        <div id="grillz" className="justify-start text-8xl lg:text-8xl text-black font-[900] font-['Archivo'] uppercase leading-[60px] lg:leading-[80px]">
-          GRILLZ
-        </div>
-        <div className="hidden lg:block w-60 h-6 justify-end text-blue text-sm font-[800] font-['Archivo']">
-          item description
-        </div>
-      </div>
+    <section id="grillz" className="self-stretch flex flex-col justify-start items-start gap-6">
+      {/* Mobile & Desktop Container */}
+      <div className="self-stretch h-[44.75rem] lg:h-[47.3rem] relative">
+        {/* Mobile Layout - Hidden on Desktop */}
+        <div className="lg:hidden w-full h-full relative">
+          {/* Grillz Header */}
+          <h2 className="absolute left-0 top-[26.75rem] text-4xl text-black font-black font-['Archivo'] uppercase leading-9">
+            Grillz
+          </h2>
 
-      {/* Unified Canvas - Mobile: Flex stacked, Desktop: Absolute positioned */}
-      <div className="self-stretch flex flex-col gap-6 lg:relative lg:min-h-[700px] lg:pt-12">
-        {/* Image 1 - Top Left Area */}
-        <Link href="/grillz" className="block lg:absolute lg:left-[8%] lg:top-[5%] lg:w-[22%] hover:scale-105 transition-transform cursor-pointer">
-          <Image
-            src="/grillzsection/plainopen.avif"
-            alt="Grillz Design 1"
-            width={348}
-            height={194}
-            className="w-full h-auto"
-          />
-        </Link>
-
-        {/* Image 2 - Top Right Area */}
-        <Link href="/grillz" className="block lg:absolute lg:right-[5%] lg:top-0 lg:w-[35%] hover:scale-105 transition-transform cursor-pointer">
-          <Image
-            src="/grillzsection/playstation1.avif"
-            alt="Grillz Design 2"
-            width={481}
-            height={223}
-            className="w-full h-auto"
-          />
-        </Link>
-
-        {/* Image 3 - Center Area */}
-        <Link href="/grillz" className="block lg:absolute lg:left-[35%] lg:top-[25%] lg:w-[18%] hover:scale-105 transition-transform cursor-pointer">
-          <Image
-            src="/grillzsection/curvy.avif"
-            alt="Grillz Design 3"
-            width={223}
-            height={103}
-            className="w-full h-auto"
-          />
-        </Link>
-
-        {/* CTA Text - Left Center */}
-        <div className="flex flex-col gap-2 lg:absolute lg:left-0 lg:top-[50%]">
-          <Link href="/grillz" className="text-4xl lg:text-6xl text-contrast font-[800] font-['Archivo'] uppercase leading-tight lg:whitespace-nowrap transition-colors cursor-pointer">
-          EXPLORE
-          </Link>
+          {/* EXPLORE CTA */}
           <Link
             href="/grillz"
-            className="text-lg font-[800] font-['Archivo'] text-contrast transition-colors"
+            className="absolute left-0 top-[29.875rem] text-4xl text-contrast font-black font-['Archivo'] uppercase leading-9 hover:opacity-80 transition-opacity"
           >
+            EXPLORE
+          </Link>
 
+          {/* Mobile Images */}
+          {/* Image 8 - Top Left with rotation */}
+          <Link href="/grillz" className="absolute w-[15rem] h-auto left-[-1.25rem] top-[12.625rem] origin-top-left rotate-[20.62deg] hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image8.avif"
+              alt="Grillz Design 8"
+              width={239}
+              height={121}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 6 - Top Right */}
+          <Link href="/grillz" className="absolute w-[15rem] h-auto left-[8rem] top-[6.375rem] hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image6.avif"
+              alt="Grillz Design 6"
+              width={240}
+              height={140}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 9 - Bottom Left */}
+          <Link href="/grillz" className="absolute w-[20rem] h-auto left-[1.0625rem] top-[35.8125rem] hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image9.avif"
+              alt="Grillz Design 9"
+              width={320}
+              height={118}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 4 - Bottom Right with 180° rotation */}
+          <Link href="/grillz" className="absolute w-[14rem] h-auto left-[25.6875rem] top-[32.5625rem] origin-top-left rotate-180 hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image4.avif"
+              alt="Grillz Design 4"
+              width={221}
+              height={138}
+              className="w-full h-auto"
+            />
           </Link>
         </div>
 
-        {/* Image 4 - Bottom Left */}
-        <Link href="/grillz" className="block lg:absolute lg:left-[8%] lg:bottom-[8%] lg:w-[20%] hover:scale-105 transition-transform cursor-pointer">
-          <Image
-            src="/grillzsection/tribal.avif"
-            alt="Grillz Design 4"
-            width={269}
-            height={155}
-            className="w-full h-auto"
-          />
-        </Link>
+        {/* Desktop Layout - Hidden on Mobile */}
+        <div className="hidden lg:block w-full h-full relative">
+          {/* EXPLORE CTA - Desktop */}
+          <Link
+            href="/grillz"
+            className="absolute left-0 top-[24rem] xl:top-[25.875rem] text-5xl xl:text-6xl 2xl:text-7xl text-contrast font-black font-['Archivo'] uppercase leading-tight xl:leading-[5rem] hover:opacity-80 transition-opacity"
+          >
+            EXPLORE
+          </Link>
 
-        {/* Image 5 - Bottom Right */}
-        <Link href="/grillz" className="block lg:absolute lg:right-[12%] lg:bottom-[5%] lg:w-[32%] hover:scale-105 transition-transform cursor-pointer">
-          <Image
-            src="/grillzsection/tree.avif"
-            alt="Grillz Design 5"
-            width={427}
-            height={170}
-            className="w-full h-auto"
-          />
-        </Link>
+          {/* Desktop Images with Responsive Breakpoints */}
+
+          {/* Image 2 - Center with rotation */}
+          <Link href="/grillz" className="absolute w-[16rem] xl:w-[18rem] 2xl:w-[19rem] h-auto left-[23rem] xl:left-[25.25rem] top-[12rem] xl:top-[13.5rem] origin-top-left rotate-[10.91deg] hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image2.avif"
+              alt="Grillz Design 2"
+              width={306}
+              height={171}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 8 - Top Left */}
+          <Link href="/grillz" className="absolute w-[20rem] xl:w-[22rem] 2xl:w-[23.25rem] h-auto left-0 top-[3rem] xl:top-[3.8125rem] hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image8.avif"
+              alt="Grillz Design 8"
+              width={372}
+              height={189}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 6 - Top Right */}
+          <Link href="/grillz" className="absolute w-[23rem] xl:w-[25rem] 2xl:w-[26.75rem] h-auto left-[40rem] xl:left-[44rem] top-0 hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image6.avif"
+              alt="Grillz Design 6"
+              width={428}
+              height={250}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 3 - Right with rotation */}
+          <Link href="/grillz" className="absolute w-[14rem] xl:w-[15rem] 2xl:w-[16rem] h-auto left-[52rem] xl:left-[56.35rem] top-[14rem] xl:top-[15.625rem] origin-top-left rotate-[37.87deg] hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image3.avif"
+              alt="Grillz Design 3"
+              width={257}
+              height={179}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 9 - Bottom Left */}
+          <Link href="/grillz" className="absolute w-[26rem] xl:w-[28rem] 2xl:w-[29.875rem] h-auto left-[0.9375rem] top-[34rem] xl:top-[36rem] hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image9.avif"
+              alt="Grillz Design 9"
+              width={478}
+              height={176}
+              className="w-full h-auto"
+            />
+          </Link>
+
+          {/* Image 4 - Bottom Right with 180° rotation */}
+          <Link href="/grillz" className="absolute w-[28rem] xl:w-[30rem] 2xl:w-[31.9375rem] h-auto left-[68rem] xl:left-[75.1875rem] top-[50.9375rem] origin-top-left rotate-180 hover:scale-105 transition-transform">
+            <Image
+              src="/grillzsection/image4.avif"
+              alt="Grillz Design 4"
+              width={511}
+              height={319}
+              className="w-full h-auto"
+            />
+          </Link>
+        </div>
       </div>
     </section>
   );
